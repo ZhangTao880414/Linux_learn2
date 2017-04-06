a56ef95cbcf72966bc10ec451c960637699bb10e
ZhangTao880414 <1785058042@qq.com>
2017-04-07 05:51:45 +0800

    添加book中后台管里富文本编辑器添加图片上传功能

--- Linux_learn2/urls.py
+++ Linux_learn2/urls.py
@@ -21,7 +21,7 @@ from blog.upload import upload_image
 
 
 urlpatterns = [
-    url(r"^uploads/(?P<path>.*)$", "django.views.static.serve", {"document_root": settings.MEDIA_ROOT,}),
+    url(r"^uploads/(?P<path>.*)$", "django.views.static.serve", {"document_root": settings.MEDIA_ROOT, }),
     url(r'^admin/upload/(?P<dir_name>[^/]+)$', upload_image, name='upload_image'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('VideoHome.urls')),
