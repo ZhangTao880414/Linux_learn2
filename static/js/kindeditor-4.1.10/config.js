ab2d7e6808dce5e316385890f98ece3f89654f8e
ZhangTao880414 <1785058042@qq.com>
2017-04-07 05:03:44 +0800

    添加book中富文本编辑器

--- static/js/kindeditor-4.1.10/config.js
+++ static/js/kindeditor-4.1.10/config.js
@@ -1,7 +1,12 @@
 KindEditor.ready(function(K) {
-                K.create('textarea[name=content]',{
+                K.create('textarea',{
                     width:'800px',
                     height:'200px',
                     uploadJson: '/admin/upload/kindeditor',
                 });
+                // K.create('textarea[name=content]',{
+                //     width:'800px',
+                //     height:'200px',
+                //     uploadJson: '/admin/upload/kindeditor',
+                // });
         });
